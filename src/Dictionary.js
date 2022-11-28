984ae7ab20b91fbfb000fb176ba70d8e406e22de
RenataMatias <112482631+RenataMatias@users.noreply.github.com>
2022-11-28 18:02:53 +0000

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    //https://dictionaryapi.dev/
+    //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -20,13 +23,11 @@ export default function Dictionary() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Search..."
-        autoFocus={true}
-        onChange={updateSearch}
-      />
-    </form>
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Search..." onChange={updateSearch} />
+      </form>
+      <Results results={results} />
+    </div>
   );
 }
